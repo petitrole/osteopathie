714d482d655a1dda3f1d9e348a62c9c79565f69f
petitrole <celinegarcher@gmail.com>
2015-10-17 20:44:35 +0200

    contact form

--- sites/all/themes/osteopathie/templates/contact-site-form.tpl.php
+++ sites/all/themes/osteopathie/templates/contact-site-form.tpl.php
@@ -11,11 +11,11 @@
 
 <div class="container">
 <div class='row top-mid-buffer'>
-<div id="bloc-footer-info" class="col-xs-8 col-sm-4  pull-right">
+<div id="bloc-footer-info" class="col-xs-12 col-sm-4  pull-right">
   <?php  $block = module_invoke('block', 'block_view', '2');
     print render($block['content']);
   ?>
-  <div id="social-icon" class="col-xs-8 col-sm-4 col-md-8  ">
+  <div id="social-icon" class="col-xs-12 col-sm-4 col-md-8  ">
     <img class="vcenter" src="<?php print $base_url . '/sites/all/themes/osteopathie/images/viadeo.png'; ?>"/>
     <img class="vcenter" src="<?php print $base_url . '/sites/all/themes/osteopathie/images/facebook.png'; ?>"/>
     <img class="vcenter" src="<?php print $base_url . '/sites/all/themes/osteopathie/images/google.png'; ?>"/>
@@ -27,7 +27,7 @@
 
 
 
-    <div class='col-xs-8 col-sm-4 col-md-6 contact-element name'>
+    <div class='col-xs-12 col-sm-4 col-md-6 contact-element name'>
       <?php print drupal_render($form['name']); ?>
     </div>
     <?php if (isset($errors['name'])): ?>
@@ -39,18 +39,18 @@
 
 
   <div class=''>
-    <div class='col-xs-8 col-sm-4 col-md-6 contact-element'>
+    <div class='col-xs-12 col-sm-4 col-md-6 contact-element'>
       <?php print drupal_render($form['prenom']); ?>
     </div>
     <?php if (isset($errors['prenom'])): ?>
-      <div class='col-xs-8 col-sm-4 contact-error'>
+      <div class='col-xs-12 col-sm-4 contact-error'>
         <span class='form-control span-error' id="error-prenom"><?php print $errors['prenom']; ?></span>
       </div>
     <?php endif; ?>
   </div>
 
   <div class=''>
-    <div class='col-xs-8 col-sm-4 col-md-6 contact-element'>
+    <div class='col-xs-12 col-sm-4 col-md-6 contact-element'>
       <?php print drupal_render($form['mail']); ?>
     </div>
     <?php if (isset($errors['mail'])): ?>
@@ -58,7 +58,7 @@
         <span class='form-control span-error' id="error-mail"><?php print $errors['mail']; ?></span>
       </div>
     <?php else : ?>
-      <div class='col-xs-8 col-sm-4 col-md-8 contact-error div-error-vide'>
+      <div class='col-xs-12 col-sm-4 col-md-8 contact-error div-error-vide'>
 		<span id='email-error' class='form-control span-error span-error-vide'>
 
 		</span>
@@ -67,7 +67,7 @@
   </div>
 
   <div class=''>
-    <div class='col-xs-8 col-sm-4  col-md-6 contact-element'>
+    <div class='col-xs-12 col-sm-4  col-md-6 contact-element'>
       <?php print drupal_render($form['message']); ?>
     </div>
     <?php if (isset($errors['message'])): ?>
