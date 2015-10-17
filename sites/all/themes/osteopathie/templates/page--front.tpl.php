0bc41d001fb087f8a1ef09adab41794c2f6d0e41
petitrole <celinegarcher@gmail.com>
2015-10-17 19:15:41 +0200

    style

--- sites/all/themes/osteopathie/templates/page--front.tpl.php
+++ sites/all/themes/osteopathie/templates/page--front.tpl.php
@@ -96,7 +96,8 @@
     </div>
 
     <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse menunav">
+      <div id="menunav">
+      <div class="navbar-collapse collapse">
         <nav role="navigation">
           <?php if (!empty($primary_nav)): ?>
             <?php print render($primary_nav); ?>
@@ -115,6 +116,7 @@
             ?>
           </div>
         </nav>
+      </div>
       </div>
     <?php endif; ?>
   </div>
